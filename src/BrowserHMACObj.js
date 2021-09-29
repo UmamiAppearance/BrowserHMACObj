8ae2e33988c692eb643547937d98835fa4a0da65
UmamiAppearance <mail@umamiappearance.eu>
2021-09-29 23:05:19 +0200

    BaseEx integration

--- src/BrowserHMACObj.js
+++ src/BrowserHMACObj.js
@@ -1,3 +1,5 @@
+import { BaseEx } from "../lib/BaseEx/src/BaseEx.js";
+
 class HMACObj {
     constructor(algorithm="SHA-256") {
         const algorithms = ["SHA-1", "SHA-256", "SHA-384", "SHA-512"];
@@ -8,7 +10,7 @@ class HMACObj {
         const version = String(algorithm).match(/[0-9]+/)[0];
         algorithm = `SHA-${version}`;
         if (!algorithms.includes(algorithm)) {
-            throw new Error(`Ivalid algorithm.\nValid arguments are: "${algorithms.join(", ")}".`);
+            throw new Error(`Invalid algorithm.\nValid arguments are: "${algorithms.join(", ")}".`);
         }
 
         // set the block-size (64 for SHA-1 & SHA-256 / 128 for SHA-384 & SHA-512)
@@ -19,13 +21,6 @@ class HMACObj {
         this.key = null;
         this.keyIsExportable = false;
         this.signature = null;
-
-        this.converters = {
-            base32: new Base32("rfc4648")
-        }
-
-        // generate the conversions for user-input
-        this.genToBufferConversions();
     }
 
     convertInput(key, type) {
@@ -41,11 +36,11 @@ class HMACObj {
             if (type === "str") {
                 msgEnc = new TextEncoder().encode(key);
             } else if (type === "hex") {
-                msgEnc = this.conversions.hexStr(key);
+                msgEnc = this.converters.base16(key);
             } else if (type === "base32") {
-                this.converters.base32.decode(key);
+                msgEnc = this.converters.base32_rfc4648.decode(key);
             } else if (type === "base64") {
-                msgEnc = Uint8Array.from(window.atob(key), c => c.charCodeAt(0));
+                msgEnc = this.converters.base64.decode(key);
             } else {
                 throw new TypeError("Unknown input type.")
             }
@@ -156,51 +151,24 @@ class HMACObj {
         return this.appendObjConversions(signatureObj);
     }
 
-    genToBufferConversions() {
-        this.conversions = { 
-            hexStr: function(hexString) {
-                /*
-                    inspired by:
-                    https://gist.github.com/don/871170d88cf6b9007f7663fdbc23fe09
-                */
-               
-                // remove the leading 0x if present
-                hexString = hexString.replace(/^0x/, '');
-                
-                if (isNaN(parseInt(hexString, 16))) {
-                    throw new TypeError("The provided input is not a valid hexadecimal string.")
-                }
-
-                // ensure even number of characters
-                if (Boolean(hexString.length % 2)) {
-                    hexString = "0".concat(hexString);
-                }
-                
-                // Split the string into pairs of octets, convert to integers 
-                // and create a Uin8array from the output.
-                const array = Uint8Array.from(hexString.match(/../g).map(pair => parseInt(pair, 16)));
-                
-                return array;
-            }   
-        }
-    }
-
-    appendObjConversions(obj) {    
-        /* 
-            The following conversion functions are
-            appended to exported key or signature.
+    appendObjConversions(obj) {
+        /*
+            Appends BaseEx encoders to the returned object for the ability
+            to covert the byte array to many representations.
         */
+
         if (!obj.array) throw new Error("No signature associated to this object.");
 
-        obj.toASCII = () => obj.array.map(b => String.fromCharCode(b)).join("");
-        obj.toBin = () => obj.array.map(b => b.toString(2).padStart(8, "0")).join("");
-        obj.toDec = () => obj.array.map(b => b.toString(10)).join("");
-        obj.toHex = () => obj.array.map(b => b.toString(16).padStart(2, "0")).join("");
-        obj.toBase32 = () => this.converters.base32.encode(obj.array, "array");
-        obj.toBase64 = () => window.btoa(obj.toASCII());
-        obj.toInt = () => parseInt(obj.toDec());
+        const capitalize = str => str.charAt(0).toUpperCase().concat(str.slice(1));
+
+        obj.toHex = () => this.converters.base16.encode(obj.array);
+        const converters = Object.keys(this.converters).slice(1);
+        for (const converter of converters) {
+            obj[`to${capitalize(converter)}`] = () => this.converters[converter].encode(obj.array);
+        }
 
         return obj;
+        
     }
 
     warnUser(message) {
@@ -212,134 +180,6 @@ class HMACObj {
     }
 }
 
+HMACObj.prototype.converters = new BaseEx("bytes");
 
-// from: https://github.com/UmamiAppearance/BaseExJS
-class Base32 {
-    constructor(standard=null) {
-        
-        if (standard && !(standard === "rfc3548" || standard === "rfc4648")) {
-            throw new TypeError("Unknown standard.\nThe options are 'rfc3548' and 'rfc4648'.");
-        }
-
-        this.standard = standard;
-
-        this.chars = {
-            rfc3548: "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567",
-            rfc4648: "0123456789ABCDEFGHIJKLMNOPQRSTUV" 
-        }
-    }
-
-    validateArgs(args) {
-        if (Boolean(args.length)) {
-            const validArgs = ["rfc3548", "rfc4648", "str", "array"];
-            const globalStandard = Boolean(this.standard);
-            const warning = this.warnUser;
-
-            args.forEach(arg => {
-                if (!validArgs.includes(arg)) {
-                    throw new TypeError(`Invalid argument: "${arg}"\nThe options are 'rfc3548' and 'rfc4648' for the rfc-standard, for in- and output-type, valid arguments are 'str' and 'array'.`);
-                } else if (validArgs.slice(0, 2).includes(arg)) {
-                    warning(`Standard is already set.\nArgument '${arg}' will be ignored.`)
-                }
-            });
-        }
-    }
-
-    validateInput(input, inputType) {
-        if (inputType === "str") {
-            if (typeof input !== "string") {
-                this.warnUser("Your input was converted into a string.");
-            }
-            return String(input);
-        } else {
-            if (typeof input === "string") {
-                throw new TypeError("Your provided input is a string, but some kind of (typed) Array is expected.");
-            } else if (typeof input !== 'object') {
-                throw new TypeError("Input must be some kind of (typed) Array if input type is set to 'array'.");
-            }
-            return input; 
-        }
-    }
-    
-    encode(input, ...args) {
-        
-        this.validateArgs(args);
-        
-        let standard = "rfc4648";
-        if (this.standard) {
-            standard = this.standard;
-        } else if (args.includes("rfc3548")) {
-            standard = "rfc3548";
-        }
-
-        const inputType = (args.includes("array")) ? "array" : "str";
-        input = this.validateInput(input, inputType);
-
-        const chars = this.chars[standard];
-
-        let binaryStr;
-        if (inputType === "str") {
-            binaryStr = input.split('').map((c) => c.charCodeAt(0).toString(2).padStart(8, "0")).join("");
-        } else if (inputType === "array") {
-            binaryStr = Array.from(input).map(b => b.toString(2).padStart(8, "0")).join("");
-        }
-
-        const bitGroups = binaryStr.match(/.{1,40}/g);
-
-        let output = "";
-        bitGroups.map(function(group) {
-            const blocks = group.match(/.{1,5}/g).map(s=>s.padEnd(5, '0'));
-            blocks.map(function(block) {
-                const charIndex = parseInt(block, 2);
-                output = output.concat(chars[charIndex]);
-            });
-        });
-        const missingChars = output.length % 8;
-        if (Boolean(missingChars)) {
-            output = output.padEnd(output.length + 8-missingChars, "=");
-        }
-
-        return output;
-    }
-
-    decode(input, ...args) {
-
-        this.validateArgs(args);
-        let standard = "rfc4648";
-        if (this.standard) {
-            standard = this.standard;
-        } else if (args.includes("rfc3548")) {
-            standard = "rfc3548";
-        }
-
-        const outputType = (args.includes("array")) ? "array" : "str";
-        const chars = this.chars[standard];
-        
-        let binaryStr = "";
-
-        input.split('').map((c) => {
-            const index = chars.indexOf(c);
-            console.log(index);
-            if (index > -1) {                                       // -1 is the index if the char was not found, "=" will be ignored
-                binaryStr = binaryStr.concat(index.toString(2).padStart(5, "0"));
-            }
-        });
-        
-        const byteArray = binaryStr.match(/.{8}/g).map(bin => parseInt(bin, 2))
-        const uInt8 = Uint8Array.from(byteArray);
-
-        if (outputType === "array") {
-            return uInt8;
-        } else {
-            return byteArray.map(b => String.fromCharCode(b)).join("");
-        }
-    }
-
-    warnUser(message) {
-        if (console.hasOwnProperty("warn")) {
-            console.warn(message);
-        } else {
-            console.log(`___\n${message}\n`);
-        }
-    }
-}
+export default HMACObj; 
