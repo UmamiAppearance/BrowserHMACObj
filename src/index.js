2e79afbe72ed56c67e62117a827314696c2e429d
UmamiAppearance <mail@umamiappearance.eu>
2022-12-09 22:14:12 +0100

    v0.1.2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 /**
  * [BrowserHMACObj]{@link https://github.com/UmamiAppearance/BrowserHMACObj}
  *
- * @version 0.1.1
+ * @version 0.1.2
  * @author UmamiAppearance [mail@umamiappearance.eu]
  * @license GPL-3.0
  */
